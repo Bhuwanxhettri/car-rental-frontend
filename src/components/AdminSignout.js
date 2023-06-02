8b54692b2a6e0d4f9add6fe8cca69e0c29e4ce7c
Bhuwanxhettri <you@example.com>
2023-06-02 18:52:09 +0545

    first commit

---
+++ src/components/AdminSignout.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AdminSignout = () => {
+  const URL = process.env.REACT_APP_BASE_URL;
+
+  const history = useNavigate();
+
+  useEffect(() => {
+    fetch(`${URL}/adminsignout`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    })
+      .then((res) => {
+        localStorage.removeItem("Admin");
+        history("/signin", { replace: true });
+        if (res.status != 200) {
+          const error = new Error(res.error);
+          throw error;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
+  return (
+    <>
+      <h1>Log Out</h1>
+    </>
+  );
+};
+
+export default AdminSignout;
